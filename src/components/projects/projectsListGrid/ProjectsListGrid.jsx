66441c1c10376f52256597a529827ad2326cedcc
ThePushMaker <elmcwl@gmail.com>
2025-05-08 00:46:44 -0700

    fix: ProjectListGrid.jsx

--- src/components/projects/projectsListGrid/ProjectsListGrid.jsx
+++ src/components/projects/projectsListGrid/ProjectsListGrid.jsx
@@ -3,43 +3,29 @@ import usePortfolioContext from "@/hooks/usePortfolioContext";
 
 const ProjectsListGrid = ({ maxProjectsToShow=6, selectedCategoryName="all" }) => {
   
-  const { projectsJSON: categoriesData } = usePortfolioContext();
+  const { projectsJSON: { categories } } = usePortfolioContext();
   
-  let displayedProjects = [];
-
-  if (selectedCategoryName !== "all") {
-    // display only projects from the selected category
-    const category = categoriesData.categories.find(
-      category => category.category === selectedCategoryName
-    );
-    displayedProjects = category?.projects.slice(0, maxProjectsToShow) || [];
-  } else {
-    // display all projects from all categories
-    displayedProjects = categoriesData.categories.flatMap( category =>
-      category.projects.map(project => ({ 
-        ...project, 
-        categoryName: category.category 
-      }))
-    ).slice(0, 9999);
-    
-  }
+  const displayedProjects = selectedCategoryName === "all"
+    ? categories.flatMap(({category, projects}) => 
+        projects.map(project => ({ ...project, categoryName: category }))
+      )
+    : (categories.find(({ category }) => category === selectedCategoryName)?.projects || [])
+      .slice(0, maxProjectsToShow)
+      .map(project => ({ ...project, categoryName: selectedCategoryName }));
   
   return (
     <div className="grid grid-cols-1 gap-4 lg:max-w-screen-lg mx-auto">
     {/* listado de proyectos */}
-      {displayedProjects.map((project, index) => {
-
-        return (
-          // card de cada proyecto individual
-          <ProjectItemCard
-            key = { index }
-            projectData = {{ ...project }}
-          />
+      {displayedProjects.map((project, index) => (
+        // card de cada proyecto individual
+        <ProjectItemCard
+          key = { index }
+          projectData = {{ ...project }}
+        />
           //  border-slate-600 group-hover:border-slate-500
-        )
-      })}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default ProjectsListGrid;
